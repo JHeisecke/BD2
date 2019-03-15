b1b6e7b6b9bb71c6395f7aad2d5abe9a0814dae8
Javier Heisecke <javierheiseckee@gmail.com>
2019-03-15 20:48:33 -0300

    Se termina el script 1 de la parte 3

--- TP_Parcial1/Scripts/PARTE_03.sql
+++ TP_Parcial1/Scripts/PARTE_03.sql
@@ -1,9 +1,11 @@
 --3.1 (sin terminar)
+--mas actualizado
 create materialized view V_SOCIOS
 build immediate
 refresh start with (sysdate) next (trunc(sysdate)+1) + 22/24
-as select S.ID_SOCIO Id_socio, S.CEDULA Cedula, S.NOMBRE||' '||S.APELLIDO as "Nombre y Apellido",
-CA.SALDO_DISPONIBLE Disponible, SUM(CASE (CP.ESTADO WHEN 'A' THEN CC.MONTO_CUOTA ELSE 0)) Deuda, SUM(CASE (SC.TIPO_OBLIGACION WHEN 'A' THEN SC.Total_A_Abonar ELSE 0)) Aporte
+as select S.ID_SOCIO Id_socio, S.CEDULA Cedula, S.NOMBRE||' '||S.APELLIDO as "Nombre y Apellido", sum(CA.SALDO_DISPONIBLE) Disponible, 
+SUM(CASE CP.ESTADO WHEN 'A' THEN CC.MONTO_CUOTA ELSE 0 end) Deuda, 
+SUM(CASE SC.TIPO_OBLIGACION WHEN 'A' THEN SC.Total_A_Abonar ELSE 0 end) Aporte
 from SOC_SOCIO S join AHO_CUENTA_AHORRO CA
 ON S.ID_SOCIO = CA.ID_SOCIO
 JOIN CRE_SOLICITUD_PRESTAMOS CSP ON
@@ -11,13 +13,10 @@ S.ID_SOCIO = CSP.SOCIO_DEUDOR
 JOIN CRE_PRESTAMOS CP ON
 CSP.ID_SOL_CRED = CP.ID_SOL_CRED
 JOIN CRE_CUOTAS CC ON
-CPS.NRO_PRESTAMO = CC.NRO_PRESTAMO
+CP.NRO_PRESTAMO = CC.NRO_PRESTAMO
 JOIN SOC_OBLIGACIONES SC ON
-SC.ID_SOCIO = S.ID_SOCIO;
---(que pasa si no tiene deudas o si su estado no es A? usar NVL(NULL, 0) en cc monto_cuota?)
---(preguntar que si no cumple aun asi debe insertarse)
---(no necesariamente dice que la cuota debe estar vencida, entonces esta demas?)
---(donde esta aporte?)
+SC.ID_SOCIO = S.ID_SOCIO
+group by S.id_socio, S.cedula, S.NOMBRE||' '||S.APELLIDO;
 
 
 ----MONTO CUOTAS ---NO PAGADAS---?????
