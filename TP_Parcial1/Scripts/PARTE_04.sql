ab7bbfb9e64e6387c8e077aa13e6c5df62240da5
Javier Heisecke <javierheiseckee@gmail.com>
2019-03-16 20:14:41 -0300

    Se agrega los nombres de la tabla en el granteo de select

--- TP_Parcial1/Scripts/PARTE_04.sql
+++ TP_Parcial1/Scripts/PARTE_04.sql
@@ -9,4 +9,7 @@ SELECT 'GRANT INSERT, UPDATE, DELETE ON '|| TABLE_NAME ||' TO javierheisecke'
 FROM tabs
 WHERE TABLE_NAME LIKE 'CRE%';
 
+SELECT 'GRANT SELECT TO '|| TABLE_NAME ||'javierheisecke'
+FROM tabs;
+
 spool off
