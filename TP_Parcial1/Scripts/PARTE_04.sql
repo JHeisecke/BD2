713bec4c92e05a75a08a8d33285d5db9493146a9
Javier Heisecke <javierheiseckee@gmail.com>
2019-03-16 19:59:34 -0300

    Para creacion de usuario y granteo

---
+++ TP_Parcial1/Scripts/PARTE_04.sql
@@ -0,0 +1,12 @@
+create user javierheisecke identified by javierheisecke	
+       DEFAULT TABLESPACE TPBD2
+       TEMPORARY TABLESPACE TEMP;
+GRANT CONNECT TO javierheisecke WITH ADMIN OPTION;
+
+spool C:\oraclexe\app\oracle\oradata\granteo.sql
+
+SELECT 'GRANT INSERT, UPDATE, DELETE ON '|| TABLE_NAME ||' TO javierheisecke'
+FROM tabs
+WHERE TABLE_NAME LIKE 'CRE%';
+
+spool off
